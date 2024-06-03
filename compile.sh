487026ae917b3cb4b39cfbbd091e01095b0f2d22
Avrum Kline <klineavrum@gmail.com>
2024-06-03 13:04:40 -0700

    :D

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-gcc *.c  -L lib/ -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL lib/libraylib.a -O3 -o build/game
+gcc *.c -o build/chess  -L lib/ -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL lib/libraylib.a -O3
 ./run.sh
